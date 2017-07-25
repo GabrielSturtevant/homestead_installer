895cbeb275fba5c4b6ae4ab8565de0cddd5f68f1
Gabriel Sturtevant <gsturtevant87@gmail.com>
2017-07-25 14:27:03 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ubuntu Homestead/Laravel  installation script
+
+This script is for my own personal use. Use it with caution as I have not written it with systems other than my own in mind
+
+The master branch will be tailored to the current Ubuntu LTS release
+
+Current LTS: 16.04
+
+
+This script can be executed via a curl command. It will run and install laravel and homestead to default locations, but if you need to modify these defaults, you can use environment variables. The following is an example of this functionality:
+```
+export SCRIPTS_URL='git@github.com:csun-metalab/etd-v3.git' && export SCRIPTS_URL_NAME='etdv3' && export SCRIPTS_DIR_NAME='ETD-V3' && export SCRIPTS_DIR_PATH='Code/Meta' && export SCRIPTS_CORES='2' && curl https://raw.githubusercontent.com/GabrielSturtevant/scripts/master/ubuntuHomestead.py | python3
+```
---
+++ ubuntuHomestead.py
@@ -0,0 +1,271 @@
+#!/usr/bin/python3
+import getopt
+import getpass
+import hashlib
+import json
+import os
+import shutil
+import subprocess
+import sys
+import time
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+# Environment Variables
+ENV_VARS = [
+	'SCRIPTS_URL',
+	'SCRIPTS_URL_NAME',
+	'SCRIPTS_DIR_NAME',
+	'SCRIPTS_DIR_PATH',
+	'SCRIPTS_CORES'
+]
+
+# Program constants
+FILE_HASH = "66bedeb9271515f1714a70ee857b51a6"
+STATIC_IP = "192.168.10.10"
+INITIAL_PATH = os.getcwd()
+HOST_PERMISSIONS = "644"
+HOMESTEAD_URL = 'https://github.com/laravel/homestead.git'
+VAGRANT_URL = 'https://raw.githubusercontent.com/GabrielSturtevant/homestead_installer/master/GetVagrantLink.py'
+
+# User Definable Variables
+SSH_LINK = False
+INSTALLED_SSH = False
+FRAMEWORK_PATH = "Code"
+URL_NAME = "homestead.app"
+DEFAULT_FRAMEWORK_URL = "https://github.com/laravel/laravel.git"
+DEFAULT_FW_DIR_NAME = 'Laravel'
+NUMBER_OF_CPUS = '1'
+
+# System modifications
+sys.stdin = open('/dev/tty')
+
+
+def md5(fname):
+	hash_md5 = hashlib.md5()
+	with open(fname, "rb") as f:
+		for chunk in iter(lambda: f.read(4096), b""):
+			hash_md5.update(chunk)
+	return hash_md5.hexdigest()
+
+
+def install(program_name, common_name):
+	print('Checking whether {} is installed'.format(common_name))
+	if shutil.which(program_name) is None:
+		print('{} is not installed, installing now'.format(common_name))
+		os.system('sudo apt install {} -y'.format(program_name))
+	else:
+		print('{} is already installed'.format(common_name))
+
+	time.sleep(1)
+	os.system('clear')
+
+
+help_out = 'This provides a small list of command line arguments that this \n' \
+		   'program will accept:\n' \
+		   '\t-u\tEnter a custom url to fetch a Laravel repository from\n' \
+		   '\t-n\tEnter a custom application name\n' \
+		   '\t-d\tEnter a custom directory path to install Laravel in\n' \
+		   '\t-D\tEnter a custom name for your Laravel directory\n' \
+		   '\t-c\tEnter the number of cores to assign to your VM\n' \
+		   '\t-h\tHelp screen'
+
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "hu:n:l:d:D:c:", ['help'])
+except getopt.GetoptError:
+	print('names.py -f -l -e -d -n <number of names to print>')
+	sys.exit(2)
+
+for opt, arg in opts:
+	if opt in ('-h', '--help'):
+		print(help_out)
+		sys.exit(1)
+	elif opt in '-u':
+		DEFAULT_FRAMEWORK_URL = arg
+		if 'git@github.com' in DEFAULT_FRAMEWORK_URL:
+			SSH_LINK = True
+	elif opt in '-n':
+		URL_NAME = arg + '.app'
+	elif opt in '-D':
+		DEFAULT_FW_DIR_NAME = arg
+	elif opt in '-d':
+		FRAMEWORK_PATH = arg
+	elif opt in '-c':
+		NUMBER_OF_CPUS = arg
+
+for x in ENV_VARS:
+	if x in os.environ:
+		if x == 'SCRIPTS_URL':
+			DEFAULT_FRAMEWORK_URL = os.getenv(x)
+			if 'git@github.com' in DEFAULT_FRAMEWORK_URL:
+				SSH_LINK = True
+		elif x == 'SCRIPTS_URL_NAME':
+			URL_NAME = os.getenv(x) + '.app'
+		elif x == 'SCRIPTS_DIR_NAME':
+			DEFAULT_FW_DIR_NAME = os.getenv(x)
+		elif x == 'SCRIPTS_DIR_PATH':
+			FRAMEWORK_PATH = os.getenv(x)
+		elif x == 'SCRIPTS_CORES':
+			NUMBER_OF_CPUS = os.getenv(x)
+
+os.system('clear')
+
+print('Running Ubuntu Homestead installation script')
+
+# Update system
+print('Updating system')
+os.system('sudo apt update')
+
+time.sleep(1)
+os.system('clear')
+
+# Ensures dependencies are met
+install('curl', 'Curl')
+install('git', 'Git')
+install('virtualbox', 'VirtualBox')
+install('vim', 'Vim')
+install('python-pip', 'Pip')
+
+# Install python dependencies
+os.system('sudo pip install beautifulsoup4')
+os.system('sudo pip install requests')
+os.system('sudo pip install lxml')
+
+# Checks whether the user has configured an ssh key
+if not os.path.isfile(os.environ['HOME'] + '/.ssh/id_rsa.pub'):
+	os.system('clear')
+	print('ssh key has not been configured.')
+	email = input('Please enter your email address (aids in generating ssh key):\n')
+	os.system('ssh-keygen -f ~/.ssh/id_rsa -t rsa -b 4096 -C "{}" -N ""'.format(email))
+else:
+	INSTALLED_SSH = True
+
+ssh_accepted = False
+
+ssh_key = open(os.environ['HOME'] + '/.ssh/id_rsa.pub', 'r').read()
+
+# TODO: Add prompt to exchange ssh key with github
+
+print("You will need to add this ssh key to github")
+
+while not ssh_accepted:
+	user_name = input('What\'s your github username? ')
+	password = getpass.getpass('What\'s your github password? ')
+	auth = HTTPBasicAuth(user_name, password)
+
+	key = ssh_key
+	url = 'https://api.github.com/user/keys'
+
+	data = {
+		'title': 'Omen',
+		'key': key,
+	}
+
+	fooo = requests.post(url, data=json.dumps(data), auth=auth)
+	print(fooo.status_code)
+	errors = None
+	foobar = json.loads(fooo.text)
+
+	if foobar.status_code == 201:
+		ssh_accepted = True
+		os.system('clear')
+		print('Congratulations! Your ssh key has been successfully added to GitHub.')
+	else:
+		os.system('clear')
+		try:
+			print('GitHub returned an error: ' + foobar['errors'][0]['message'])
+		except NameError:
+			print('An unknown error occurred')
+		print('Your username and/or password were incorrect\n')
+
+time.sleep(1)
+os.system('clear')
+
+os.system('rm -rf vagrant*.deb')
+
+time.sleep(1)
+file_name = 'temp.py'
+python_program = open(file_name, 'w+')
+r = requests.get(VAGRANT_URL)
+python_program.write(r.content.decode('utf-8'))
+python_program.close()
+
+if md5(file_name) == FILE_HASH:
+	os.system('wget $(python {})'.format(file_name))
+else:
+	print('Python script integrity compromised. Exiting now')
+	exit(1)
+
+os.system('rm -f {}'.format(file_name))
+
+os.system('sudo dpkg -i vagrant*.deb')
+
+os.system('rm -rf vagrant*.deb')
+
+print('Attempting to edit /etc/hosts, a backup will be created at /etc/hosts.BAK')
+new_hosts = open('hosts', 'w+')
+old_hosts = open('/etc/hosts', 'r')
+new_hosts.write(old_hosts.read())
+
+old_hosts.close()
+os.system('sudo cp /etc/hosts /etc/hosts.BAK')
+
+to_write = "\n# Homestead ip address and url\n"
+to_write += "{}\t{}".format(STATIC_IP, URL_NAME)
+new_hosts.write(to_write)
+new_hosts.close()
+
+os.system('sudo mv hosts /etc/hosts')
+os.system('sudo chmod {} /etc/hosts'.format(HOST_PERMISSIONS))
+print('Finished editing hosts file')
+
+# Go to home directory
+os.chdir(os.environ['HOME'])
+
+os.system('git clone ' + HOMESTEAD_URL + ' Homestead')
+
+path = FRAMEWORK_PATH
+
+while True:
+	path = path.split('/')
+	if path[0] == '':
+		path.pop()
+
+	try:
+		os.makedirs(os.path.join(os.environ['HOME'], *path))
+		FRAMEWORK_PATH = '/'.join(path)
+		break
+	except FileExistsError:
+		print('Oops, looks like that directory already exists\n')
+		path = input('Please enter a new path to place the framework'
+					 ' in \n(type N to place it in existing directory):\n')
+		if path.lower() == 'n':
+			break
+
+os.chdir('{}/{}'.format(os.environ['HOME'], FRAMEWORK_PATH))
+os.system('git clone {} {}'.format(DEFAULT_FRAMEWORK_URL, DEFAULT_FW_DIR_NAME))
+
+os.chdir(os.environ['HOME'] + '/Homestead')
+os.system('chmod +x init.sh; ./init.sh')
+
+homestead_yaml = open('Homestead.yaml', 'r+')
+new_yaml = open('Homestead.yaml.new', 'w+')
+info = homestead_yaml.readlines()
+
+for line in info:
+	if 'Code' in line:
+		line = line.replace('Code', '{}'.format(FRAMEWORK_PATH))
+
+	if 'Laravel' in line:
+		line = line.replace('Laravel', '{}'.format(DEFAULT_FW_DIR_NAME))
+
+	if 'cpus: 1' in line:
+		line = line.replace('1', NUMBER_OF_CPUS)
+
+	if 'homestead.app' in line:
+		line = line.replace('homestead.app', URL_NAME)
+	new_yaml.write(line)
+os.system('rm Homestead.yaml; mv Homestead.yaml.new Homestead.yaml')
+
+os.chdir(INITIAL_PATH)
