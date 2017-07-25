48627b8d585c34f47fa5cef10783933e0b3b67ba
Gabriel Sturtevant <gsturtevant87@gmail.com>
2017-07-25 14:38:23 -0700

    Added warning

--- GetVagrantLink.py
+++ GetVagrantLink.py
@@ -1,6 +1,28 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+#
+# homestead_installer
+# Copyright (C) 2017, Gabriel Sturtevant <gabriel@gabrielsturtevant.com>
+#
+# This file is part of homestead_installer.
+#
+# homestead_installer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# homestead_installer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with homestead_installer.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contributor(s):
+# Gabriel Sturtevant <gabriel@gabrielsturtevant.com>
 from bs4 import BeautifulSoup
 import requests
+import sys
 
 url = "https://www.vagrantup.com/downloads.html"
 
@@ -13,4 +35,5 @@ soup = BeautifulSoup(data, 'lxml')
 for link in soup.find_all('a'):
     linkText = link.get('href')
     if 'deb' in linkText and 'x86_64' in linkText:
-        print linkText
+        print(linkText)
+        sys.exit(0)
--- ubuntuHomestead.py
+++ ubuntuHomestead.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
+#
+# homestead_installer
+# Copyright (C) 2017, Gabriel Sturtevant <gabriel@gabrielsturtevant.com>
+#
+# This file is part of homestead_installer.
+#
+# homestead_installer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# homestead_installer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with homestead_installer.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contributor(s):
+# Gabriel Sturtevant <gabriel@gabrielsturtevant.com>
+
 import getopt
 import getpass
 import hashlib
